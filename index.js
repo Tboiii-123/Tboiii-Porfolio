1db7c72c20a6c58260fbfc77d33c78f3d6d72be0
Tboiii-123 <lawalhussein775@gmail.com>
2025-01-21 00:04:23 +0100

    commited

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const observer = new IntersectionObserver ((entries)=>{
             entry.target.classList.add('show');
         }
         
-        else{
-            entry.target.classList.remove('show');
-        }
+        // else{
+        //     entry.target.classList.remove('show');
+        // }
+
         
     });
 });
@@ -19,7 +20,31 @@ const observer = new IntersectionObserver ((entries)=>{
 
 
 
-
 const hiddenElemnt =document.querySelectorAll('.hidden');
 
 hiddenElemnt.forEach((el) => observer.observe(el));
+
+
+const hiddenElemnt2 =document.querySelectorAll('.hid');
+
+hiddenElemnt2.forEach((el) => observer.observe(el));
+
+
+
+
+
+const text = "Hello!!!";
+const textOutput = document.getElementById('text-output');
+let index=0;
+
+    function typeLetterByLetter() {
+      if (index < text.length) {
+        textOutput.textContent += text[index];
+        index++;
+        setTimeout(typeLetterByLetter, 120);
+      }
+    }
+  
+    // Start typing effect
+    typeLetterByLetter();
+
