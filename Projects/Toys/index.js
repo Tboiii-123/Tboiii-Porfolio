8493d89880a75a225e2dba7b0909a875e9d077cb
Tboiii-123 <lawalhussein775@gmail.com>
2025-06-21 22:54:25 +0100

    commited

---
+++ Projects/Toys/index.js
@@ -0,0 +1,24 @@
+
+const observer = new IntersectionObserver ((entries)=>{
+
+    entries.forEach((entry) => {
+
+        console.log(entry)
+        
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+        
+    });
+});
+
+
+
+
+
+const hiddenElemnt =document.querySelectorAll('.hidden');
+
+hiddenElemnt.forEach((el) => observer.observe(el));
